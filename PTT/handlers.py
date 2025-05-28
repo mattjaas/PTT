451eb7da41bed943cfa6d9207e0aa11a51511efe
mattjaas <183701493+mattjaas@users.noreply.github.com>
2025-05-28 02:01:26 +0200

    Update handlers.py

--- PTT/handlers.py
+++ PTT/handlers.py
@@ -43,8 +43,8 @@ def handle_site_before_title(context):
 
     # wzorzec całej domeny z pl/com.pl, www. i wieloma poddomenami
     domain_pattern = (
-        r'(?:www\.)?[\w_-]+(?:\.[\w_-]+)*'
-        r'(?:\.(?:com\.)?pl|[\s_-]pl)'
+        r'(?:www\.)?[\w-]+(?:\.[\w-]+)*'
+        r'(?:\.(?:com\.)?pl|[\s-]pl)'
     )
 
     # 1) Bracketed: [domena.pl]  lub  {domena.com.pl}  albo  (domena pl)
@@ -59,7 +59,7 @@ def handle_site_before_title(context):
     # 2) Bez nawiasu, ale natychmiast potem '-' lub ' -'
     if not m:
         m = regex.search(
-            rf'^({domain_pattern})\s*[-_]\s*',
+            rf'^({domain_pattern})\s*-\s*',
             text,
             regex.IGNORECASE
         )
